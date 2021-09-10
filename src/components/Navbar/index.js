78e51e99b6eb76300e03a07ad4e5635329acbf58
Parth Patel <parth6796@yahoo.com>
2021-09-10 15:52:39 +0100

    minor changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,27 @@
 import React from "react";
+//React fontawesome imports
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
   return (
    //     <div className="container">
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light ">
 
       <div className="container">
         <a className="navbar-brand" href="#">
           {
-              <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              fill="#fff"
-              className="bi bi-laptop"
-              viewBox="0 0 16 16"
-              >
-              <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z" />
-            </svg>
+            //   <svg
+            //   xmlns="http://www.w3.org/2000/svg"
+            //   width="30"
+            //   height="30"
+            //   fill="#fff"
+            //   className="bi bi-laptop"
+            //   viewBox="0 0 16 16"
+            //   >
+            //   <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z" />
+            // </svg>
+            <img className="page-logo" src="https://svgsilh.com/svg/1191372.svg" alt="" />
           }
         </a>
         <button
@@ -29,7 +33,7 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
           >
-          <span className="navbar-toggler-icon bg-dark"></span>
+          <FontAwesomeIcon icon={faBars} style= {{color: "grey"}}/>
         </button>
         <div className="collapse navbar-collapse mr-auto" id="navbarSupportedContent">
           <div className=" d-flex justify-content-end">
