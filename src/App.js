c9de0f90ed9b026c331861a0f45716b4b473235a
Parth Patel <parth6796@yahoo.com>
2021-09-11 14:58:58 +0100

    added background header floaters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
 import {Navbar, Header} from './components'
+import Particles from "react-particles-js";
 
 const App = () => {
     return (
         <>
+            <Particles 
+                params={{
+                    particles:{
+                    number: {
+                        value: 30,
+                        density:{
+                            enable:true,
+                            value_area: 900
+                        }
+                    },
+                    shape:{
+                        type: "circle",
+                        stroke: {
+                            width:6,
+                            color: "#f9ab00"
+                    }
+                }}
+                }}/>
             <Navbar />
             <Header />
         </>
