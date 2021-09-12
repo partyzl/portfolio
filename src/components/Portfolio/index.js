b528c6f11a956c9d11c597b125d20710fa27e321
Parth Patel <parth6796@yahoo.com>
2021-09-12 11:39:31 +0100

    added filler text for cards

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <>
+      <div className="portfolio-body" id="portfolio">
+        <div className="container">
+          <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+              <div class="card">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos dignissimos autem dicta earum laboriosam molestiae temporibus officiis corrupti aliquam similique!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam maiores itaque corporis, veniam aspernatur ullam reprehenderit consequuntur quo facere blanditiis.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error commodi explicabo inventore vel voluptatibus ratione qui mollitia consequatur ullam veniam.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam enim sequi illum error ipsa? Ipsam atque tenetur recusandae fugiat dicta.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Portfolio;
