03687ed07d77fbe4c36774d5086920cc2a0b796f
Parth Patel <parth6796@yahoo.com>
2021-09-12 13:48:35 +0100

    added message on submit contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Contact = () => {
+
+    const [message, setMessage] = useState(false);
+
+    const emptyForm = (e) => {
+        e.target.email.value="";
+        e.target.subject.value="";
+        e.target.msg.value="";
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+        emptyForm();
+    }
+
     return(
         <>
             <div className="contact-body" id="contact-me">
@@ -8,10 +23,12 @@ const Contact = () => {
                     <div className="col-lg-6"></div>
                     <div className="col-lg-6 form-boundary">
                         <h2>Contact Me</h2>
-                            <form>
-                                <input className="col-md-10" type="email" placeholder="Your Email Here"/>
-                                <input className="col-md-8" type="text" placeholder="Subject" />
-                                <textarea className="col-md-10" type="text" placeholder= "How can I help you?" rows="5"/>
+                            <form onSubmit={handleSubmit}>
+                                <input className="col-md-10" type="email" name="email" required placeholder="Your Email Here"/>
+                                <input className="col-md-8" type="text" name="subject" placeholder="Subject" />
+                                <textarea className="col-md-10" type="text" name="msg" required placeholder= "How can I help you?" rows="5"/>
+                                <button type="submit"className="form-btn">Submit</button><br/>
+                                {message && <span className="form-msg">Thank you, I will reply ASAP!</span>}
                             </form>
                     </div>
                 </div>
